08c89cc2ac9bce300fe15773fd1de330971b7f7f
Alvaro R <alvaroruiz27@correo.ugr.es>
2026-09-25 10:53:10 +0200

    validate_tm: clear the Keras session between checkpoints to avoid GPU OOM

--- QC/validate_tm.py
+++ QC/validate_tm.py
@@ -56,6 +56,7 @@ import re
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import keras.backend as K
 
 # project imports
 from QC.predict_tm import predict_tm
@@ -141,6 +142,8 @@ def validate_training(image_dir,
                        min_vox=min_vox,
                        recompute=True,
                        verbose=False)
+            # free the previous checkpoint's graph to avoid running out of GPU memory
+            K.clear_session()
 
 
 def read_scores(path_csv, column='abs_err', subjects=None):
